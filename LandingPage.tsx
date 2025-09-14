ce4e01d23c2ea1eb09a135d9b203be48d3715ec4
Samit Bhisikar <samitbhisikar16@gmail.com>
2025-09-14 15:39:47 +0530

    Update LandingPage.tsx

--- LandingPage.tsx
+++ LandingPage.tsx
@@ -1,1 +1,15 @@
 
+import * as React from "react";
+import { Header } from "./Header";
+import { HeroSection } from "./HeroSection";
+
+export function LandingPage() {
+  return (
+    <div className="flex overflow-hidden flex-col px-7 pt-11 pb-28 bg-neutral-950 max-md:px-5 max-md:pb-24">
+      <Header />
+      <HeroSection />
+    </div>
+  );
+}
+
+export default LandingPage;
